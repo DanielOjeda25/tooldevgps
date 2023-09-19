674168eaf72860ab1241d76db5e4efdaac1b5d9b
DanielOjeda25 <ojedadanielalejandro333@gmail.com>
2023-09-19 10:52:00 -0300

    changes

--- src/components/InputJson.jsx
+++ src/components/InputJson.jsx
@@ -1,9 +1,11 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import '../styles/Input.css';
 
 const InputJson = ({ onJsonChange }) => {
   // Estado para almacenar el valor del input
   const [inputValue, setInputValue] = useState('');
+  // Estado para mostrar una advertencia
+  const [showWarning, setShowWarning] = useState(false);
 
   // Función para manejar cambios en el input
   const handleInputChange = (event) => {
@@ -17,20 +19,35 @@ const InputJson = ({ onJsonChange }) => {
     if (event.keyCode === 13) {
       // Evita que se realice un salto de línea en el input
       event.preventDefault();
-      onJsonChange(inputValue);
-      setInputValue('');
+
+      // Verifica si el valor del input está vacío
+      if (inputValue.trim() === '') {
+        // Muestra la advertencia
+        setShowWarning(true);
+
+        // Oculta la advertencia después de 3 segundos
+        setTimeout(() => {
+          setShowWarning(false);
+        }, 3000); // 3000 milisegundos (3 segundos)
+      } else {
+        // Oculta la advertencia y procesa el JSON
+        setShowWarning(false);
+        onJsonChange(inputValue);
+        setInputValue('');
+      }
     }
   };
 
   return (
     <div className="input-container">
-      <input
-        type="text"
-        placeholder="Ingrese sus puntos..."
+      <textarea
+        rows="5"
+        placeholder="Ingrese sus coordenadas..."
         value={inputValue}
         onChange={handleInputChange}
         onKeyDown={handleEnterKey}
       />
+      {showWarning && <p className="warning">El campo no puede estar vacío.</p>}
     </div>
   );
 }
